239bf3c0f8c5ab474a63255b56f2709fa3fd5b72
demonuchy <demonuchy@gmail.com>
2026-04-07 11:45:59 +0300

    v 1.1.8

--- src/admin/setup.py
+++ src/admin/setup.py
@@ -11,12 +11,22 @@ from shared.config import config
 class AdminSetup:
     """Класс управления админ панелью и представлениями"""
     def __init__(self, app, engine):
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        templates_path = os.path.join(current_dir, "my_templates")
+        
+        #print(f"Current file location: {current_dir}")
+        #print(f"Templates path: {templates_path}")
+        #print(f"Templates path exists: {os.path.exists(templates_path)}")
+        #print(f"sqladmin subdir exists: {os.path.exists(os.path.join(templates_path, 'sqladmin'))}")
+        #print(f"create.html exists: {os.path.exists(os.path.join(templates_path, 'sqladmin', 'create.html'))}")
+        
         self.admin = Admin(
             app, 
             engine, 
             title="Veterans Admin", 
             base_url="/api/v2/admin", 
-            authentication_backend=AuthBackend(secret_key=config.ADMIN_SECRET_TOKEN)
+            authentication_backend=AuthBackend(secret_key=config.ADMIN_SECRET_TOKEN),
+            templates_dir=templates_path
             )
         self._custom_views: List[Type[ModelView]] = [
             UserAdmin, 
